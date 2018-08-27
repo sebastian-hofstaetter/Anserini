8de31711ff258a6cfef5358cbfb96b4aaabb8c74
Peilin Yang <peiliny@twitter.com>
2018-08-26 21:10:25 -0700

    minor tweak

--- src/main/java/io/anserini/index/IndexCluewebRelated.java
+++ src/main/java/io/anserini/index/IndexCluewebRelated.java
@@ -32,10 +32,8 @@ import org.apache.lucene.util.BytesRef;
 import org.kohsuke.args4j.*;
 
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.Executors;
@@ -44,9 +42,9 @@ import java.util.concurrent.TimeUnit;
 
 public final class IndexCluewebRelated {
   private static final Logger LOG = LogManager.getLogger(IndexCluewebRelated.class);
-
+  
   public static final class Args {
-
+    
     // required arguments
     @Option(name = "-docid2url", metaVar = "[Path]", required = true, usage = "Document Id TO URL mapping file. See: " +
         "http://www.lemurproject.org/clueweb12/related-data.php")
@@ -54,7 +52,7 @@ public final class IndexCluewebRelated {
     
     @Option(name = "-index", metaVar = "[Path]", required = true, usage = "index path")
     public String index;
-  
+    
     @Option(name = "-threads", metaVar = "[Number]", required = true, usage = "Number of Threads")
     public int threads;
     
@@ -62,15 +60,15 @@ public final class IndexCluewebRelated {
     @Option(name = "-pagerank", metaVar = "[Path]", usage = "Pagerank score file. See: " +
         "http://www.lemurproject.org/clueweb09/pageRank.php and http://www.lemurproject.org/clueweb12/PageRank.php")
     public String pagerankInput;
-  
+    
     @Option(name = "-spamscore", metaVar = "[Path]", usage = "Spam score file. See: " +
         "https://www.mansci.uwaterloo.ca/~msmucker/cw12spam/")
     public String spamscoreInput;
-  
+    
     @Option(name = "-webgraph", metaVar = "[Path]", usage = "Webgraph file. See: " +
         "http://www.lemurproject.org/clueweb09/webGraph.php and http://www.lemurproject.org/clueweb12/webgraph.php/")
     public String webgraphInput;
-  
+    
     @Option(name = "-url2nodeid", metaVar = "[Path]", usage = "The Url TO NodeId file for Webgraph. See: " +
         "http://www.lemurproject.org/clueweb09/webGraph.php and http://www.lemurproject.org/clueweb12/webgraph.php/")
     public String url2NodeidInput;
@@ -125,18 +123,21 @@ public final class IndexCluewebRelated {
    * @throws IOException
    */
   private Map<String, String> readUrl2DocIdMapping(String path) throws IOException {
+    LOG.info("Reading " + path);
     Map<String, String> res = new HashMap<>();
     InputStream in = getReadFileStream(path);
     BufferedReader bRdr = new BufferedReader(new InputStreamReader(in));
     String line;
+    long cnt = 0;
     while ((line = bRdr.readLine()) != null) {
       String[] lineSplit = line.split("\\s+");
       String docid = lineSplit[0];
       String url = lineSplit[1];
-      Map<String, Object> obj = values.getOrDefault(docid, new HashMap<String, Object>());
-      obj.put(FIELD_URL, url);
-      values.put(docid, obj);
       res.put(url, docid);
+      cnt++;
+      if(cnt % 1e5 == 0) {
+        LOG.info(String.format("Read %d lines", cnt));
+      }
     }
     return res;
   }
@@ -148,16 +149,22 @@ public final class IndexCluewebRelated {
    * @throws IOException
    */
   private Map<Long, String> readNodeId2DocIdMapping(String path, Map<String, String> url2DocIdMapping) throws IOException {
+    LOG.info("Reading " + path);
     Map<Long, String> res = new HashMap<>();
     InputStream in = getReadFileStream(path);
     BufferedReader bRdr = new BufferedReader(new InputStreamReader(in));
     String line;
+    long cnt = 0;
     while ((line = bRdr.readLine()) != null) {
       String[] lineSplit = line.split("\\s+");
       String url = lineSplit[0];
       long nodeid = Long.parseLong(lineSplit[1]);
       if (url2DocIdMapping.containsKey(url)) {
         res.put(nodeid, url2DocIdMapping.get(url));
+      }
+      cnt++;
+      if(cnt % 1e5 == 0) {
+        LOG.info(String.format("Read %d lines", cnt));
       }
     }
     return res;
@@ -170,6 +177,7 @@ public final class IndexCluewebRelated {
    * @throws IOException
    */
   private void readWebGraph(String path, Map<Long, String> nodeId2DocIdMapping) throws IOException {
+    LOG.info("Reading " + path);
     InputStream in = getReadFileStream(path);
     BufferedReader bRdr = new BufferedReader(new InputStreamReader(in));
     String line;
@@ -192,6 +200,9 @@ public final class IndexCluewebRelated {
           values.put(thisDocId, thisObj);
         }
       }
+      if(i % 1e5 == 0) {
+        LOG.info(String.format("Read %d lines", i));
+      }
     }
   }
   
@@ -200,9 +211,11 @@ public final class IndexCluewebRelated {
    * @throws IOException
    */
   private void readPagerankScores(String path) throws IOException {
+    LOG.info("Reading " + path);
     InputStream in = getReadFileStream(path);
     BufferedReader bRdr = new BufferedReader(new InputStreamReader(in));
     String line;
+    long cnt = 0;
     while ((line = bRdr.readLine()) != null) {
       String[] lineSplit = line.split("\\s+");
       String docid = lineSplit[0];
@@ -210,6 +223,10 @@ public final class IndexCluewebRelated {
       Map<String, Object> obj = values.getOrDefault(docid, new HashMap<String, Object>());
       obj.put(FIELD_PAGERANK, (double)obj.getOrDefault(FIELD_PAGERANK, 0.0) + pagerank);
       values.put(docid, obj);
+      cnt++;
+      if(cnt % 1e5 == 0) {
+        LOG.info(String.format("Read %d lines", cnt));
+      }
     }
   }
   
@@ -218,41 +235,60 @@ public final class IndexCluewebRelated {
    * @throws IOException
    */
   private void readSpamScores(String path) throws IOException {
-    InputStream in = getReadFileStream(path);
-    BufferedReader bRdr = new BufferedReader(new InputStreamReader(in));
-    String line;
-    while ((line = bRdr.readLine()) != null) {
-      String[] lineSplit = line.split("\\s+");
-      double spam = Double.parseDouble(lineSplit[0]);
-      String docid = lineSplit[1];
-      Map<String, Object> obj = values.getOrDefault(docid, new HashMap<String, Object>());
-      obj.put(FIELD_SPAM, spam);
-      values.put(docid, obj);
-    }
+    LOG.info("Reading " + path);
+        Files.walkFileTree(Paths.get(path), new FileVisitor<Path>() {
+      @Override
+      public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+        return FileVisitResult.CONTINUE;
+      }
+      @Override
+      public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        InputStream in = getReadFileStream(path);
+        BufferedReader bRdr = new BufferedReader(new InputStreamReader(in));
+        String line;
+        while ((line = bRdr.readLine()) != null) {
+          String[] lineSplit = line.split("\\s+");
+          double spam = Double.parseDouble(lineSplit[0]);
+          String docid = lineSplit[1];
+          Map<String, Object> obj = values.getOrDefault(docid, new HashMap<String, Object>());
+          obj.put(FIELD_SPAM, spam);
+          values.put(docid, obj);
+        }
+        return FileVisitResult.CONTINUE;
+      }
+      @Override
+      public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+        return FileVisitResult.CONTINUE;
+      }
+      @Override
+      public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+        return FileVisitResult.CONTINUE;
+      }
+    });
   }
   
   private final class IndexerThread extends Thread {
     private String docid;
     private Map<String, Object> values;
     final private IndexWriter writer;
-
+    
     private IndexerThread(IndexWriter writer, String docid, Map<String, Object> values) throws IOException {
       this.docid = docid;
       this.writer = writer;
       this.values = values;
     }
-
+    
     @Override
     public void run() {
       try {
         // Make a new, empty document.
         Document document = new Document();
-  
+        
         // Store the collection docid.
         document.add(new StringField("id", docid, Field.Store.YES));
         // This is needed to break score ties by docid.
         document.add(new SortedDocValuesField("id", new BytesRef(docid)));
-  
+        
         for(Map.Entry<String, Object> ent : values.entrySet()) {
           String k = ent.getKey();
           if (k == FIELD_URL) {
@@ -281,10 +317,10 @@ public final class IndexCluewebRelated {
       }
     }
   }
-
+  
   private final IndexCluewebRelated.Args args;
   private final Path indexPath;
-
+  
   public IndexCluewebRelated(IndexCluewebRelated.Args args) throws Exception {
     this.args = args;
     LOG.info("Index path: " + args.index);
@@ -294,19 +330,19 @@ public final class IndexCluewebRelated {
     LOG.info("spamscoreInput path: " + args.spamscoreInput);
     LOG.info("webgraphInput path: " + args.webgraphInput);
     LOG.info("url2NodeidInput path: " + args.url2NodeidInput);
-
+    
     this.indexPath = Paths.get(args.index);
     if (!Files.exists(this.indexPath)) {
       Files.createDirectories(this.indexPath);
     }
   }
-
+  
   public void run() throws IOException, InterruptedException {
     final long start = System.nanoTime();
     LOG.info("Starting indexer...");
-
+    
     int numThreads = args.threads;
-
+    
     final Directory dir = FSDirectory.open(indexPath);
     final IndexWriterConfig config = new IndexWriterConfig();
     config.setSimilarity(new BM25Similarity());
@@ -321,7 +357,7 @@ public final class IndexCluewebRelated {
       executor.execute(new IndexerThread(writer, ent.getKey(), ent.getValue()));
     }
     executor.shutdown();
-
+    
     try {
       // Wait for existing tasks to terminate
       while (!executor.awaitTermination(1, TimeUnit.MINUTES)) {
@@ -334,9 +370,9 @@ public final class IndexCluewebRelated {
       // Preserve interrupt status
       Thread.currentThread().interrupt();
     }
-
+    
     int numIndexed = writer.maxDoc();
-
+    
     try {
       writer.commit();
       writer.forceMerge(1);
@@ -349,16 +385,16 @@ public final class IndexCluewebRelated {
         LOG.error(e);
       }
     }
-
+    
     final long durationMillis = TimeUnit.MILLISECONDS.convert(System.nanoTime() - start, TimeUnit.NANOSECONDS);
     LOG.info(String.format("Total %,d documents indexed in %s", numIndexed,
         DurationFormatUtils.formatDuration(durationMillis, "HH:mm:ss")));
   }
-
+  
   public static void main(String[] args) throws Exception {
     IndexCluewebRelated.Args indexCollectionArgs = new IndexCluewebRelated.Args();
     CmdLineParser parser = new CmdLineParser(indexCollectionArgs, ParserProperties.defaults().withUsageWidth(90));
-
+    
     try {
       parser.parseArgument(args);
     } catch (CmdLineException e) {
@@ -368,7 +404,7 @@ public final class IndexCluewebRelated {
           parser.printExample(OptionHandlerFilter.REQUIRED));
       return;
     }
-
+    
     new IndexCluewebRelated(indexCollectionArgs).run();
   }
 }
